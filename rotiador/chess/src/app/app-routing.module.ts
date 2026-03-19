5820ca9e8aeb06f6d899ec89fa097f007d4104ad
“LaysArceles” <LaysArceles12@gmail.com>
2026-03-19 13:11:02 -0300

    inserir shared e features

--- rotiador/chess/src/app/app-routing.module.ts
+++ rotiador/chess/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './features/main-page/main-page.component';
+import { LoginPageComponent } from './features/login-page/login-page.component';
+import { NotFoundPageComponent } from './features/not-found-page/not-found-page.component';
+import { MercadoPageComponent } from './features/main-page/mercado-page/mercado-page.component';
+import { ComidaPageComponent } from './features/main-page/comida-page/comida-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "",component:MainPageComponent, children:[
+    {path:"Merdadinho",component:MercadoPageComponent},
+    {path:"comida", component:ComidaPageComponent}
+  ]},
+  {path: "login",component:LoginPageComponent},
+  {path: "**",component:NotFoundPageComponent},
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
